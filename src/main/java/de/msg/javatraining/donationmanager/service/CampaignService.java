1a8416e6fd79ef05d59361bda1b70a7ff8afb1c6
Alexia Fratila <alexia.fratila@msg.group>
2023-08-17 11:14:50 +0300

    BE from old repo

---
+++ src/main/java/de/msg/javatraining/donationmanager/service/CampaignService.java
@@ -0,0 +1,39 @@
+package de.msg.javatraining.donationmanager.service;
+
+import de.msg.javatraining.donationmanager.persistence.model.Campaign;
+import de.msg.javatraining.donationmanager.persistence.repository.impl.CampaignRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CampaignService {
+
+    @Autowired
+    CampaignRepositoryImpl campaignRepository;
+
+    public List<Campaign> findAll() {
+        List<Campaign> campaigns = campaignRepository.findAll();
+        return campaigns;
+    }
+
+    public Campaign findById(Long id) {
+        return campaignRepository.findById(id);
+    }
+
+    public Campaign create(Campaign campaign) {
+        campaignRepository.create(campaign);
+        return campaign;
+    }
+
+    public Campaign update(Long id, Campaign updateCampaign) {
+        campaignRepository.update(id, updateCampaign);
+        return updateCampaign;
+    }
+
+    public void delete(Long id, Campaign campaign) {
+        campaignRepository.delete(campaign);
+    }
+
+}
