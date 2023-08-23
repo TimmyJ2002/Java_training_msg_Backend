d4b8e26dc24153899ba999272b82d3873a6bc004
timonjanosimsg <139860309+timonjanosimsg@users.noreply.github.com>
2023-08-23 15:57:40 +0300

    Notification can be updated, read and mark them as unread + Alexia Notification older than 30 days will be deleted

--- src/main/java/de/msg/javatraining/donationmanager/service/NotificationService.java
+++ src/main/java/de/msg/javatraining/donationmanager/service/NotificationService.java
@@ -14,6 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
