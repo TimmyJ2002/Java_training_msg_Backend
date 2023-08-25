b980978066d5cf9c57c721449f1e8c894573b9a8
Simona Tivadar <simona.tivadar@msg.group>
2023-08-25 12:40:44 +0300

    implemented test classes for campaign and donation

--- src/test/java/de/msg/javatraining/donationmanager/service/DonationServiceTest.java
+++ src/test/java/de/msg/javatraining/donationmanager/service/DonationServiceTest.java
@@ -1,18 +1,24 @@
 package de.msg.javatraining.donationmanager.service;
 
+import de.msg.javatraining.donationmanager.persistence.model.Campaign;
 import de.msg.javatraining.donationmanager.persistence.model.Donation;
+import de.msg.javatraining.donationmanager.persistence.model.Donator;
+import de.msg.javatraining.donationmanager.persistence.model.User;
 import de.msg.javatraining.donationmanager.persistence.repository.impl.DonationRepositoryImpl;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 class DonationServiceTest {
 
@@ -40,5 +46,92 @@ class DonationServiceTest {
         assertEquals(2, result.size());
     }
 
-    // Add more test methods for other service methods if needed
+    @Test
+    public void testAddDonation() {
+        Donation donation = new Donation();
+        donation.setAmount(100);
+        donation.setCurrency("USD");
+
+        donationService.addDonation(donation);
+
+        verify(donationRepository, times(1)).saveDonation(donation);
+    }
+
+    @Test
+    public void testDeleteDonation() {
+        long donationId = 1L;
+
+        donationService.deleteDonation(donationId);
+
+        verify(donationRepository, times(1)).deleteDonation(donationId);
+    }
+
+
+    @Test
+    public void testUpdateDonation() {
+        int oldDonationID = 1;
+        int newAmount = 200;
+        String newCurrency = "EUR";
+        Campaign newCampaign = new Campaign();
+        Donator newDonator = new Donator();
+        String newNotes = "Updated notes";
+
+        donationService.updateDonation(oldDonationID, newAmount, newCurrency, newCampaign, newDonator, newNotes);
+
+        verify(donationRepository, times(1)).updateDonation(oldDonationID, newAmount, newCurrency, newCampaign, newDonator, newNotes);
+    }
+//    @Test
+//    public void testApproveDonation() throws ChangeSetPersister.NotFoundException {
+//        User approvedByUser = new User();
+//        approvedByUser.setUsername("testUser");
+//
+//        Donation donation = new Donation();
+//        donation.setId(1L);
+//
+//        String jwtToken = "testJwtToken";
+//        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+//        when(mockRequest.getHeader("Authorization")).thenReturn("Bearer " + jwtToken);
+//
+//        when(jwtUtils.getUserNameFromJwtToken(jwtToken)).thenReturn("testUser");
+//        when(userService.findUserByUsername("testUser")).thenReturn(approvedByUser);
+//        when(donationService.findByID(1)).thenReturn(donation);
+//
+//        donationService.approveDonation(mockRequest, 1L);
+//
+//        assertTrue(donation.isApproved());
+//        assertEquals(approvedByUser, donation.getApprovedBy());
+//        assertNotNull(donation.getApproveDate());
+//
+//        verify(donationRepositoryJPA, times(1)).save(donation);
+//    }
+//
+//    @Test
+//    public void testApproveDonationUserNotFound() throws ChangeSetPersister.NotFoundException {
+//        String jwtToken = "testJwtToken";
+//        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+//        when(mockRequest.getHeader("Authorization")).thenReturn("Bearer " + jwtToken);
+//
+//        when(jwtUtils.getUserNameFromJwtToken(jwtToken)).thenReturn("testUser");
+//        when(userService.findUserByUsername("testUser")).thenReturn(null);
+//
+//        assertThrows(ChangeSetPersister.NotFoundException.class,
+//                () -> donationService.approveDonation(mockRequest, 1L));
+//    }
+//
+//    @Test
+//    public void testApproveDonationDonationNotFound() throws ChangeSetPersister.NotFoundException {
+//        User approvedByUser = new User();
+//        approvedByUser.setUsername("testUser");
+//
+//        String jwtToken = "testJwtToken";
+//        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+//        when(mockRequest.getHeader("Authorization")).thenReturn("Bearer " + jwtToken);
+//
+//        when(jwtUtils.getUserNameFromJwtToken(jwtToken)).thenReturn("testUser");
+//        when(userService.findUserByUsername("testUser")).thenReturn(approvedByUser);
+//        when(donationService.findByID(1)).thenReturn(null);
+//
+//        assertThrows(ChangeSetPersister.NotFoundException.class,
+//                () -> donationService.approveDonation(mockRequest, 1L));
+//    }
 }
